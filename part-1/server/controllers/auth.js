e92ddb1e30146cbb354bf76dfeb90d012b91be4f
dallasmay <dmmay9@gmail.com>
2022-06-16 21:23:10 -0600

    implemented login functionality, it might be working, it might not be

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -4,55 +4,59 @@ const users = [];
 module.exports = {
     login: (req, res) => {
       console.log('Logging In User')
-      console.log(req.body)
+      // console.log(req.body)
+  
       const { username, password } = req.body
+
       for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
+        if (users[i].username === username /*&& users[i].passwordHash === password*/) {
+          if (bcrypt.compareSync(password, users[i].passwordHash, (err, result) => {
+            return result;
+          })) {
+            console.log(users[i]);
+            delete users[i].passwordHash;
+            console.log(users[i]);
+            console.log("Login Successful");
+            return res.status(200).send(users[i]);
+          } else {
+            res.status(400).send("Incorrect password");
+          } 
+          
+        } else {
+          // res.status(400).send("User not found.");
         }
       }
-      res.status(400).send("User not found.")
+      
     },
     register: (req, res) => {
         const saltRounds = 10;
         const plainTextPassword = req.body.password;
-        let newUser = {
-          username: req.body.username, 
-          email: req.body.email, 
-          firstName: req.body.firstName, 
-          lastName: req.body.lastName, 
-          passwordHash: 0
-        };
-        // newUser.passwordHash = 20;
-        // console.log(newUser.passwordHash);
+        
+        // Hashing of plainTextPassword
         bcrypt.hash(plainTextPassword, saltRounds, (err, hash) => {
           if (!err) {
             console.log(hash);
             // New user object to push to database
-            // let newUser = {
-            //   username: req.body.username, 
-            //   email: req.body.email, 
-            //   firstName: req.body.firstName, 
-            //   lastName: req.body.lastName, 
-            //   passwordHash: 0
-            // };           
+            let newUser = {
+              username: req.body.username, 
+              email: req.body.email, 
+              firstName: req.body.firstName, 
+              lastName: req.body.lastName, 
+              passwordHash: 0
+            };           
             newUser.passwordHash = hash;
             console.log(newUser)
-            res.status(200).send(console.log("Lookin' good!"));
+            res.status(200).send(req.body);
             users.push(newUser);
-            // console.log(users);
           } else {
             res.status(400).send(`Not lookin' so good, ${err}`);
           }
         })
       
-      
-      
-        console.log('asldfklsdkjf')
-        console.log(newUser);
+        // console.log(newUser);
         console.log('Registering User')
-        console.log(req.body)
+        // console.log(req.body)
         // users.push(req.body)
-        res.status(200).send(req.body)
+        // res.status(200).send(req.body)
     }
 }
